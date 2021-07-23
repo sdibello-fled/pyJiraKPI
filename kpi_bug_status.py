7d1e8e353dc6f0fffcf93719e596bb1f49781a12
sdibello-fled <63073403+sdibello-fled@users.noreply.github.com>
2021-07-23 11:11:46 -0400

    added kpi_bug_status for adding numbers to the staff meeting slides

--- kpi_bug_status.py
+++ kpi_bug_status.py
@@ -2,8 +2,6 @@ import os
 import json
 import aiohttp
 import asyncio
-import calendar
-import sys
 from datetime import timedelta, date, datetime
 from dotenv import load_dotenv
 
@@ -40,19 +38,19 @@ async def get_sprint_list(project):
 async def analyze_sprints(store):
         jsondata = await get_sprint_list(store.project)
         sprints = jsondata['sprints']
-        id_list = []
         for sprint in sprints:
                 sprint_enddate = sprint['end']    # example 08012018045900
                 sprint_id = sprint['id']
                 is_closed = sprint['closed']
                 date_time_obj = datetime.strptime(sprint_enddate, '%d%m%Y%H%M%S')
+                # if the sprint is NOT closed - add it to the active sprint list ( there should be 1 per team)
                 if is_closed == False:
                         if sprint_id not in store.sprint_black_list:
                                 store.active_sprints.append(int(sprint_id))
-                if date_time_obj >= store.end_date: 
-                        if is_closed == True:
-                                if sprint_id not in store.sprint_black_list:
-                                        store.sprints.append(int(sprint_id))
+                # if the sprint is closed, and the end date of the sprint, is greater then the end date in the store object
+                elif date_time_obj >= store.end_date: 
+                        if sprint_id not in store.sprint_black_list:
+                                store.sprints.append(int(sprint_id))
         return store
 
 async def run_generic_jql(project, jql):
@@ -65,83 +63,32 @@ async def run_generic_jql(project, jql):
                 response = json.loads(response)
         return response        
 
-
 async def bug_count(store):
         ## get a list of all bugs for a project that are no closed.
         auth = aiohttp.BasicAuth(login = os.environ.get('JIRA_USER'), password = os.environ.get('JIRA_API_KEY'))
         jql = f'project="{store.project}" and statusCategory != Done and "Zendesk Ticket Count[Number]" > 0 and type = "Bug"'
-        url = f'https://frontlinetechnologies.atlassian.net/rest/api/2/search?jql={jql}'
-        #print(url)
-        async with aiohttp.ClientSession(auth=auth) as session:
-                raw = await session.get(url) 
-                response = await raw.text()
-                response = json.loads(response)
-
-        return response                  
-
+        return await run_generic_jql(store.project, jql)
+           
 async def get_all_tickets_in_sprints(project, sprint_id_array):
         ## get a list of all sprints
         stringlist = map(str, sprint_id_array)
         listi = ",".join(stringlist)
-        auth = aiohttp.BasicAuth(login = os.environ.get('JIRA_USER'), password = os.environ.get('JIRA_API_KEY'))
         jql = f'project = "{project}" AND Sprint in ({listi}) and type in ("Support Defect", "Support Request", "bug")'
-        url = f'https://frontlinetechnologies.atlassian.net/rest/api/2/search?jql={jql}'
-        #print('jql=> ' + jql)
-        #print(url)
-        async with aiohttp.ClientSession(auth=auth) as session:
-                raw = await session.get(url)
-                response = await raw.text()
-                response = json.loads(response)
-        return response        
-
-
+        return await run_generic_jql(project, jql)
+ 
 async def get_priority_tickets_in_sprints(project, sprint_id_array, priority):
         ## get a list of all sprints
         stringlist = map(str, sprint_id_array)
         listi = ",".join(stringlist)
-        auth = aiohttp.BasicAuth(login = os.environ.get('JIRA_USER'), password = os.environ.get('JIRA_API_KEY'))
         jql = f'project = "{project}" AND Sprint in ({listi}) and type in ("Support Defect", "Support Request", "bug") And priority = {priority}'
-        url = f'https://frontlinetechnologies.atlassian.net/rest/api/2/search?jql={jql}'
-        #print('jql=> ' + jql)
-        #print(url)
-        async with aiohttp.ClientSession(auth=auth) as session:
-                raw = await session.get(url)
-                response = await raw.text()
-                response = json.loads(response)
-        return response        
+        return await run_generic_jql(project, jql)
 
 async def get_priority_tickets_not_done(project, priority):
         ## get a list of all sprints
-        auth = aiohttp.BasicAuth(login = os.environ.get('JIRA_USER'), password = os.environ.get('JIRA_API_KEY'))
         jql = f'project = "{project}" and type in ("Support Defect", "Support Request", "bug") and "Zendesk Ticket Count[Number]" > 0 and statusCategory != Done and StatusCategory = "In Progress" And priority = {priority}'
-        url = f'https://frontlinetechnologies.atlassian.net/rest/api/2/search?jql={jql}'
-        #print('jql=> ' + jql)
-        #print(url)
-        async with aiohttp.ClientSession(auth=auth) as session:
-                raw = await session.get(url)
-                response = await raw.text()
-                response = json.loads(response)
-        return response    
+        return await run_generic_jql(project, jql)
 
-async def main():
-        load_dotenv()
-        now = datetime.today()
-        enddate = now + timedelta(days=-70)
-
-        data = kpi_bug_status_store()
-        # set to how many overall team do work, needed to calculate last four sprints
-        data.teams = 2
-        data.debug = True                  
-        data.start_date = now
-        data.end_date = enddate
-        data.project = 'HCMAT'
-        #data.project = 'FC'
-        data.sprint_black_list = [3152, 1301]
-        data.sprint_white_list = []
-        data.sprints = []
-        typeArray = ['Support Defect', 'Support Request', 'Bug']
-        last_four_count = 0
-
+async def process(data):
         data = await analyze_sprints(data)
         data.sprints.sort()
         data.active_sprints.sort()
@@ -170,29 +117,20 @@ async def main():
         print(f'unresolved {data.project} tickets  {data.total_bugs_unresolved}')
 
         issues = last_two['issues']
-        last_P1_count = 0
-        last_P2_count = 0
-        last_P3_count = 0
-        last_P4_count = 0
 
+        # API fails if you try to run them all in one jql because the maxresults doesn't work on this API.. poo!
         highest =  await get_priority_tickets_in_sprints(data.project, last_complete, "Highest")
         last_P1_count = int(highest['total'])
-
         high =  await get_priority_tickets_in_sprints(data.project, last_complete, "High")
         last_P2_count = int(high['total'])
-
         Medium =  await get_priority_tickets_in_sprints(data.project, last_complete, "Medium")
         last_P3_count = int(Medium['total'])
-
         Low =  await get_priority_tickets_in_sprints(data.project, last_complete, "Low")
         last_P4_count = int(Low['total'])
 
-        print(f'last sprint resolved {data.project} P2 tickets High  {last_P1_count}')
-        print(f'last sprint resolved {data.project} P2 tickets High  {last_P2_count}')
-        print(f'last sprint resolved {data.project} P2 tickets Medium {last_P3_count}')
-        print(f'last sprint resolved {data.project} P2 tickets Low {last_P4_count}')
+        print(f'[RESOLVED] last sprint {data.project} P1 tickets P1:{last_P1_count} P2:{last_P2_count}, P3:{last_P3_count}, P4:{last_P4_count}')
 
-
+        # API fails if you try to run them all in one jql because the maxresults doesn't work on this API.. poo!
         undone_highest =  await get_priority_tickets_not_done(data.project, "Highest")
         undone_P1_count = int(undone_highest['total'])
         undone_high =  await get_priority_tickets_not_done(data.project, "High")
@@ -202,11 +140,30 @@ async def main():
         undone_low =  await get_priority_tickets_not_done(data.project, "Low")
         undone_P4_count = int(undone_low['total'])
 
+        print(f'[IN PROGRESS] {data.project} P1 tickets P1:{undone_P1_count} P2:{undone_P2_count}, P3:{undone_P3_count}, P4:{undone_P4_count}')
 
-        print(f' {data.project} P1 tickets High  {undone_P1_count}')
-        print(f' {data.project} P2 tickets High  {undone_P2_count}')
-        print(f' {data.project} P3 tickets High  {undone_P3_count}')
-        print(f' {data.project} P4 tickets High  {undone_P4_count}')
+async def main():
+        load_dotenv()
+        data = kpi_bug_status_store()
+
+        # set to how many overall team do work, needed to calculate last four sprints
+        data.teams = 2
+        data.debug = True
+        # dates reversed, start date is today, or the start date, end date is going back in time.                  
+        now = datetime.today()
+        enddate = now + timedelta(days=-70)
+        data.start_date = now
+        data.end_date = enddate
+
+        #data.project = 'HCMAT'
+        data.project = 'FC'
+        data.sprint_black_list = [3152, 1301]
+        data.sprint_white_list = []
+        data.sprints = []
+        typeArray = ['Support Defect', 'Support Request', 'Bug']
+        last_four_count = 0
+
+        await process(data)
 
 if __name__ == '__main__':
         loop = asyncio.get_event_loop()
