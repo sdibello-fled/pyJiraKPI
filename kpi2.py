ee5605c1baf7073280a2f292ff0919fd7390cb71
sdibello-fled <63073403+sdibello-fled@users.noreply.github.com>
2021-09-16 16:27:32 -0400

    added object to track projects and run everything

--- kpi2.py
+++ kpi2.py
@@ -14,8 +14,8 @@ async def main():
         mon = 8
         debugFlag = False
         trackedProjects = []
-        project = 'HCMAT'
-        rapid_view = '588'
+        #project = 'HCMAT'
+        #rapid_view = '588'
         #project = 'FC'
         #rapid_view = '464'
         hcmat = {
@@ -32,40 +32,41 @@ async def main():
         sprint_black_list = [3152]
         #TODO
         #sprints_white_list = []
-        sprint_id_list = await kpi_month.get_sprint_ids_by_month(project, year, mon, sprint_black_list) 
-        
-        #loading all the velocity reports
-        all_sprints_month = kpi_month.kpi_month(project, year, mon, debugFlag)
-        for id in sprint_id_list:
-            veloReport = jira_velocity_report(debugFlag)
-            raw_velocity = await veloReport.get_sprint_velocity_report(os.environ.get('JIRA_USER'), os.environ.get('JIRA_API_KEY'), id, rapid_view)
-            velocity = await veloReport.velocity_report_parse(raw_velocity)
-            all_sprints_month.velocity_reports.append(velocity)
+        for proj in trackedProjects:
+               sprint_id_list = await kpi_month.get_sprint_ids_by_month(proj['project'], year, mon, sprint_black_list) 
 
-        # filter the velocity reports
-        us_team = kpi_month.kpi_month(project, year, mon, debugFlag)
-        noida_team = kpi_month.kpi_month(project, year, mon, debugFlag)
-        for v1 in all_sprints_month.velocity_reports:
-                if 'US ' in v1.name: 
-                        us_team.velocity_reports.append(v1)
+               #loading all the velocity reports
+               all_sprints_month = kpi_month.kpi_month(proj['project'], year, mon, debugFlag)
+               for id in sprint_id_list:
+                   veloReport = jira_velocity_report(debugFlag)
+                   raw_velocity = await veloReport.get_sprint_velocity_report(os.environ.get('JIRA_USER'), os.environ.get('JIRA_API_KEY'), id, proj['view'])
+                   velocity = await veloReport.velocity_report_parse(raw_velocity)
+                   all_sprints_month.velocity_reports.append(velocity)
 
-                elif 'Noida ' in v1.name:
-                        noida_team.velocity_reports.append(v1)
+               # filter the velocity reports
+               us_team = kpi_month.kpi_month(proj['project'], year, mon, debugFlag)
+               noida_team = kpi_month.kpi_month(proj['project'], year, mon, debugFlag)
+               for v1 in all_sprints_month.velocity_reports:
+                       if 'US ' in v1.name: 
+                               us_team.velocity_reports.append(v1)
 
-        print('us team sprints')
-        for v2 in us_team.velocity_reports:
-                print(v2.name)
+                       elif 'Noida ' in v1.name:
+                               noida_team.velocity_reports.append(v1)
 
-        await us_team.calculate_kpis()
-        
-        us_team.print_kpis()
+               print('us team sprints')
+               for v2 in us_team.velocity_reports:
+                       print(v2.name)
 
-        print('noida team sprints')
-        for v3 in noida_team.velocity_reports:
-                print(v3.name)
-        
-        await noida_team.calculate_kpis()
-        noida_team.print_kpis()
+               await us_team.calculate_kpis()
+
+               us_team.print_kpis()
+
+               print('noida team sprints')
+               for v3 in noida_team.velocity_reports:
+                       print(v3.name)
+
+               await noida_team.calculate_kpis()
+               noida_team.print_kpis()
 
 if __name__ == '__main__':
         loop = asyncio.get_event_loop()
