2943d8d074f7c5f9d32d45295833ab1b5ba4e7c3
sdibello-fled <63073403+sdibello-fled@users.noreply.github.com>
2022-06-01 15:26:19 -0400

    updated SOC2 to run both HCMAT and FC in one go.

--- SOC2_csv.py
+++ SOC2_csv.py
@@ -17,12 +17,17 @@ async def main():
         tickets = []
         hcmat = {'project':'HCMAT'}
         fc = {'project':'FC'}
-        #trackedProjects.append(hcmat)
+        trackedProjects.append(hcmat)
         trackedProjects.append(fc)
 
+        print ("Start")
         for proj in trackedProjects:
-            data = await kpi_query.get_all_soc2_stories(proj['project'], '2021-06-01', '2022-03-31', False)
+            data = []
+            issues = []
+            projectName = proj['project']
+            data = await kpi_query.get_all_soc2_stories(projectName, '2022-04-01', '2022-05-31', False)
             # go through the list of responeses ( need to pull issues from each one.)
+            print ("Data Acquired")
             for api_response in data:
                 if api_response['issues']:
                     issues = issues + api_response['issues']
@@ -30,18 +35,19 @@ async def main():
             for issue in issues:
                 tickets.append(jira_ticket.parse_issue_type(issue, False))
         
-        rows = []
-        row = ['Ticket Key', 'Issue Type', 'Priority', 'Summary',  'Ticket Id', 'Status', 'last updated', 'created', 'assignee']
-        rows.append(row)
-        for ticket in tickets:
-            row = [str(ticket.key), str(ticket.issuetype), str(ticket.priority), str(ticket.summary).replace(",", ""), str(ticket.id), str(ticket.status), str(ticket.lastUpdated), str(ticket.createdDate), str(ticket.assignee)]
+            rows = []
+            row = ['Ticket Key', 'Issue Type', 'Priority', 'Summary',  'Ticket Id', 'Status', 'last updated', 'created', 'assignee']
             rows.append(row)
-        #for ticket in tickets:
-        #    print(f"{ticket.key},{ticket.issuetype},{ticket.priority},{ticket.summary},{ticket.priority},{ticket.id},{ticket.status},{ticket.sprint}")
+            for ticket in tickets:
+                row = [str(ticket.key), str(ticket.issuetype), str(ticket.priority), str(ticket.summary).replace(",", ""), str(ticket.id), str(ticket.status), str(ticket.lastUpdated), str(ticket.createdDate), str(ticket.assignee)]
+                rows.append(row)
 
-        with open ('soc2.csv', 'w', newline='') as csvfile:
-            spamwriter  = csv.writer(csvfile, delimiter=',', quotechar='!', quoting=csv.QUOTE_MINIMAL)
-            spamwriter.writerows(rows)
+            with open (f'soc2-{projectName}.csv', 'w', newline='') as csvfile:
+                spamwriter  = csv.writer(csvfile, delimiter=',', quotechar='!', quoting=csv.QUOTE_MINIMAL)
+                spamwriter.writerows(rows)
+            rows.clear()
+            tickets.clear()
+        print ("Done")
 
 if __name__ == '__main__':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
