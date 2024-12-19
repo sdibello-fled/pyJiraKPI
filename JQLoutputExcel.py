f5aed702520b5c6fab6e1d75e5aedee039fb8b3f
sdibello-fled <63073403+sdibello-fled@users.noreply.github.com>
2024-12-19 09:39:07 -0500

    finish SQLoutputExcel - for now

--- JQLoutputExcel.py
+++ JQLoutputExcel.py
@@ -9,17 +9,23 @@ import time
 
 ## This loops though all the history data - we shouold look for specific events to pull dates from.
 def getCreationAndResolutionDate(histories : list) -> tuple:
-        resolutiondate = None
-        createddate = None
-        for item in histories:
-                historyevents = item.get('items', [])
-                for events in historyevents:
-                      #seems like the "result" field is the one that has the resolution date we should track
-                      if events['field'] == 'resolution':
-                           resolutiondate = datetime.strptime(item['created'], '%Y-%m-%dT%H:%M:%S.%f%z')
-                           break
-        createddate = datetime.strptime(histories[-1]['created'], '%Y-%m-%dT%H:%M:%S.%f%z')
-        return (createddate, resolutiondate)
+    resolutiondate = None
+    createddate = None
+    inProgressDate = None
+    for item in histories:
+        historyevents = item.get('items', [])
+        for events in historyevents:
+            #seems like the "result" field is the one that has the resolution date we should track
+            if events['field'] == 'resolution':
+                resolutiondate = datetime.strptime(item['created'], '%Y-%m-%dT%H:%M:%S.%f%z')
+                break
+            # only process the first in progress date - as they are in reverse order this should be the most recent.
+            if inProgressDate is None:
+                if events['field'] == 'status' and events['toString'] == 'In Progress':
+                    inProgressDate = datetime.strptime(item['created'], '%Y-%m-%dT%H:%M:%S.%f%z')
+                    break
+    createddate = datetime.strptime(histories[-1]['created'], '%Y-%m-%dT%H:%M:%S.%f%z')
+    return (createddate, resolutiondate, inProgressDate)
 
 async def main(jql: str, filename: str) -> None:
     load_dotenv()
@@ -34,7 +40,7 @@ async def main(jql: str, filename: str) -> None:
     ws.title = "Jira Issues"
 
     # Write the header row
-    headers = ['Key', 'Summary', 'Status', 'Priority', 'Severity', 'Assignee', 'Parent', 'date_diff_hours', 'createdDate', 'resolutionDate']
+    headers = ['Key', 'Summary', 'Status', 'Priority', 'Severity', 'Assignee', 'Parent', 'hours_to_progress', 'date_diff_hours', 'createdDate', 'resolutionDate', 'inProgressDate']
     ws.append(headers)
 
     # Loop through the issues and write the data to the worksheet
@@ -55,10 +61,15 @@ async def main(jql: str, filename: str) -> None:
 
         dateinfo = getCreationAndResolutionDate(histories)
         if dateinfo and dateinfo[1] is not None:
-            date_difference = (dateinfo[1] - dateinfo[0]).total_seconds() / 3600
+            date_difference_create_to_res = (dateinfo[1] - dateinfo[0]).total_seconds() / 3600
         else:
-            date_difference = 0
-              
+            date_difference_create_to_res = 0
+        
+        if dateinfo and dateinfo[2] is not None:
+            date_difference_create_to_work = (dateinfo[2] - dateinfo[0]).total_seconds() / 3600
+        else:
+            date_difference_create_to_work = 0
+
         ws.append([
             issue.get('key', 'N/A'),
             fields.get('summary', 'N/A'),
@@ -68,9 +79,11 @@ async def main(jql: str, filename: str) -> None:
             severity['value'] if severity else 'N/A',
             assignee['displayName'] if assignee else 'Unassigned',
             parent['key'] if parent else 'No Parent',
-            date_difference,
+            date_difference_create_to_work,
+            date_difference_create_to_res,
             dateinfo[0].strftime('%Y-%m-%d %H:%M:%S') if dateinfo else 'N/A',
-            dateinfo[1].strftime('%Y-%m-%d %H:%M:%S') if dateinfo and dateinfo[1] else 'N/A'
+            dateinfo[1].strftime('%Y-%m-%d %H:%M:%S') if dateinfo and dateinfo[1] else 'N/A',
+            dateinfo[2].strftime('%Y-%m-%d %H:%M:%S') if dateinfo and dateinfo[2] else 'N/A'
         ])
 
     # Save the workbook to a file
@@ -80,10 +93,10 @@ async def main(jql: str, filename: str) -> None:
 
 if __name__ == '__main__':
     ## eveything created by Zendesk
-    jql = 'Project = HCMAT and creator = 557058:a01b4872-0086-4bc3-903e-23f26637afa2 and createdDate >= endOfMonth(-3)'   
+    ##jql = 'Project = HCMAT and creator = 557058:a01b4872-0086-4bc3-903e-23f26637afa2 and createdDate >= endOfMonth(-3)'   
     
     ## everything created by Zendesk or has a Zendesk ticket count greater than 0 from the last 3 or so months.
-    ###jql = 'Project = HCMAT and creator = 557058:a01b4872-0086-4bc3-903e-23f26637afa2 or "Zendesk Ticket Count[Number]" > 0 and createdDate >= endOfMonth(-3)'
+    jql = 'Project = HCMAT and creator = 557058:a01b4872-0086-4bc3-903e-23f26637afa2 or "Zendesk Ticket Count[Number]" > 0 and createdDate >= startOfMonth(-3)'
     ##jql = 'key = "HCMAT-63032"'
     file = 'C:\\data\\PyData1.xlsx'
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
